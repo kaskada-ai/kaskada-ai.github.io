a17a8453a597d733ffe20ae76554e5c28779e294
Jose Galdámez <eladio.galdamez@gmail.com>
2023-03-24 18:24:50 -0600

    Marketo newsletter

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,47 @@ function calculateWindowHeight() {
   document.head.appendChild(style);
 }
 
+function initializeNewsletter() {
+  if (MktoForms2) {
+    MktoForms2.loadForm("//pages.datastax.com", "259-IFZ-779", 4727, (form) => {
+      const $newsletter = document.querySelector("form[newsletter-form]");
+      const $button = $newsletter.querySelector('button[type="submit"]');
+
+      if ($button) {
+        $button.disabled = false;
+      }
+
+      $newsletter.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const $email = $newsletter.querySelector('input[name="email"]');
+
+        window.theform = form;
+        form.setValues({
+          Email: $email.value,
+        });
+
+        $("#newsletter-modal").modal("show");
+
+        setTimeout(() => {
+          document.querySelector("#mktoForm_4727 #Email").focus();
+        }, 500);
+      });
+    });
+  }
+}
+
+function listenNewsletterFocus() {
+  const $form = document.querySelector("form#mktoForm_4727");
+
+  if ($form) {
+    const $input = document.getElementById("newsletter-email");
+    $input.addEventListener("focus", initializeNewsletter);
+  }
+}
+
 addEventListener("DOMContentLoaded", function () {
   calculateWindowHeight();
+  listenNewsletterFocus();
 
   window.addEventListener("resize", calculateWindowHeight());
 });
